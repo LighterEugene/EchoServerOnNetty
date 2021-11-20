b7cc0d79daead619474e5333c24e60aa103ae692
LighterEugene <51925892+LighterEugene@users.noreply.github.com>
2021-11-20 14:36:11 +0300

    EchoServerHandler

--- untitled/untitled1/src/main/java/study/netty/EchoServerHandler.java
+++ untitled/untitled1/src/main/java/study/netty/EchoServerHandler.java
@@ -44,3 +44,44 @@ public class EchoServerHandler extends ChannelInboundHandlerAdapter {
     }
 }
 
+package study.netty;
+
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+Scanner msg = new Scanner(System.in);
+    String s = msg.nextLine();
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        if (s.equals("disconnection")) {
+            System.out.println("Отключения от сервера...");
+            System.exit(0);
+        }
+        ctx.writeAndFlush(Unpooled.copiedBuffer(s, StandardCharsets.UTF_8));
+    }
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+
+        logger.info("client received:{}", msg.toString(StandardCharsets.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+
+        logger.error(cause.getMessage());
+        ctx.close();
+    }
+
+}
